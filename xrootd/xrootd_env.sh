e2c5e2c905d0818fb2c101bf0e903147cc306b24
Maxim Potekhin <potekhin@bnl.gov>
2017-03-07 12:23:09 -0500

    A typo fix and a rename

--- xrootd/xrootd_env.sh
+++ xrootd/xrootd_env.sh
@@ -1,10 +1,13 @@
+# This script needs to be edited (paths changed)
+# to be applicable to a general use scenario
+#
 export HOSTNAME=`hostname`
 #
 export XROOTD_BASE=/opt/xrootd
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$XROOTD_BASE/lib
-export PATH=$PATH:$XROOTD_BASE/lib
+export PATH=$PATH:$XROOTD_BASE/bin
 #
-export XROOTD_EXPORT=/home/maxim/
+export XROOTD_EXPORT=/home/maxim/p3sdata/
 export XROOTD_AUTH_FILE=/home/maxim/auth_file/
 export XROOTD_XRDR=sagacity.local
 #
