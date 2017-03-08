aabaf839a5ef45bf8c10cd6caeeebe0171c7c566
Maxim Potekhin <potekhin@bnl.gov>
2017-03-07 19:42:41 -0500

    swapped lines

--- xrootd/xrootd_start.sh
+++ xrootd/xrootd_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cmsd    -b -l $XROOTD_LOGDIR/cmsd.log      -s $XROOTD_LOGDIR/cmsd.pid     -c $XROOTD_CFG_FILE
 xrootd  -b -l $XROOTD_LOGDIR/xrootd.log    -s $XROOTD_LOGDIR/xrootd.pid   -c $XROOTD_CFG_FILE
+cmsd    -b -l $XROOTD_LOGDIR/cmsd.log      -s $XROOTD_LOGDIR/cmsd.pid     -c $XROOTD_CFG_FILE
