c372ced7d754ff1462bd5218f39185ca8c43b353
Maxim Potekhin <potekhin@bnl.gov>
2017-08-31 09:15:38 -0400

    tweak cron

--- htcondor/pcalc.sh
+++ htcondor/pcalc.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+export P3S_HOME=/afs/cern.ch/user/m/mxp/projects/p3s
+export DQM_HOME=/afs/cern.ch/user/m/mxp/projects/dqmconfig
+
+source $P3S_HOME/configuration/p3s_lx_setup.sh
+
 if [ -z ${P3S_PILOTS+x} ];
 then exit;
 fi
 
-p=`/afs/cern.ch/user/m/mxp/projects/p3s/clients/summary.py -p`
+p=`$P3S_HOME/clients/summary.py -p`
 if [[ $p < $P3S_PILOTS ]];
 then
     echo required pilots:$P3S_PILOTS, detected:$p ;
     toSub=$(( P3S_PILOTS-p ));
     echo $toSub
-    condor_submit N=$toSub /afs/cern.ch/user/m/mxp/projects/dqmconfig/htcondor/psub.jdl
+    condor_submit N=$toSub $DQM_HOME/htcondor/psub.jdl
 fi
