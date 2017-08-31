f6a5bcc4a8e0e3c49c0789c6e0e4c76359d71da7
buddhasystem <potekhin@bnl.gov>
2017-08-31 16:32:25 +0200

    Improved logic in pilot detection

--- htcondor/pcalc.sh
+++ htcondor/pcalc.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+idle=`/usr/bin/condor_q | tail -1 | cut -d' ' -f 7`
+
+if [[ $idle > 0 ]];
+then
+    echo $idle idling condor jobs found, exiting
+    exit
+fi
+
 export P3S_HOME=/afs/cern.ch/user/m/mxp/projects/p3s
 export DQM_HOME=/afs/cern.ch/user/m/mxp/projects/dqmconfig
 
 source $P3S_HOME/configuration/p3s_lx_setup.sh
 
 if [ -z ${P3S_PILOTS+x} ];
-then exit;
+then
+    echo P3S_PILOTS undefined, exiting
+    exit
 fi
 
 source /afs/cern.ch/user/m/mxp/vp3s/bin/activate
 
 p=`$P3S_HOME/clients/summary.py -p`
+
+echo detected pilots:$p
 
 if [[ $p < $P3S_PILOTS ]];
 then
