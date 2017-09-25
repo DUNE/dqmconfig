586f61d9eb41a71d9616134b88801985ee71640f
Maxim Potekhin <potekhin@bnl.gov>
2017-09-25 13:16:51 -0400

    Improved the pilot driver

--- htcondor/pcalc.sh
+++ htcondor/pcalc.sh
@@ -12,7 +12,6 @@ export P3S_HOME=/afs/cern.ch/user/m/mxp/projects/p3s
 export DQM_HOME=/afs/cern.ch/user/m/mxp/projects/dqmconfig
 
 source $P3S_HOME/configuration/lxvm.sh
-source /afs/cern.ch/user/m/mxp/nPilots.sh
 
 if [ -z ${P3S_PILOTS+x} ];
 then
@@ -22,7 +21,7 @@ fi
 
 source /afs/cern.ch/user/m/mxp/vp3s/bin/activate
 
-to=`$P3S_HOME/clients/TO.py -t 2000`
+to=`$P3S_HOME/clients/TO.py -t $P3S_PILOT_TO`
 echo timeout:$to
 
 purge=`$P3S_HOME/clients/purge.py -w pilot -s timeout`
