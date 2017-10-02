e85dc4d2b888c5111d35a7669932e8084c4d51ad
Maxim Potekhin <potekhin@bnl.gov>
2017-10-02 19:37:18 -0400

    Fixing the bad deletion bug

--- services/olddata.sh
+++ services/olddata.sh
@@ -5,13 +5,25 @@ export DQM_HOME=/afs/cern.ch/user/m/mxp/projects/dqmconfig
 
 source $P3S_HOME/configuration/lxvm.sh
 
-outdir = $P3S_DIRPATH/output
+export outdir=$P3S_DIRPATH/output
 
-du = `du -sh $outdir`
-echo Used space: $du
+usedDisk=`du -sh $outdir`
+echo Used space: $usedDisk
 
 cd $outdir
-N=`find . -maxdepth 1 -mindepth 1 -mmin +1200 -exec ls -ld  {} \; | wc -l`
+
+curdir=`pwd`
+echo Currently in $curdir
+if [[ $curdir == /afs* ]]
+then
+    echo GOT INTO AFS, EXITING
+    exit
+fi
+    
+
+
+
+N=`find $outdir -maxdepth 1 -mindepth 1 -mmin +1200 -exec ls -ld  {} \; | wc -l`
 echo $N directories found exceeding the data lifetime of $DQM_DATA_LIFE minutes
 
-find . -maxdepth 1 -mindepth 1 -mmin +$DQM_DATA_LIFE -exec rm -fr  {} \;
+# find . -maxdepth 1 -mindepth 1 -mmin +$DQM_DATA_LIFE -exec rm -fr  {} \;
