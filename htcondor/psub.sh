6af10082b399cc59a2b95c831606a35e30f5895a
Maxim Potekhin <potekhin@bnl.gov>
2017-10-08 14:32:14 -0400

    Handling the extra HTCondor-related arguments

--- htcondor/psub.sh
+++ htcondor/psub.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+export A=''
+if [ -z "$1" ]
+  then
+    echo "No extra arguments supplied"
+    exit
+else
+    export A=$1
+fi
+
+
 source /afs/cern.ch/user/m/mxp/vp3s/bin/activate
-/afs/cern.ch/user/m/mxp/projects/p3s/clients/pilot.py
+/afs/cern.ch/user/m/mxp/projects/p3s/clients/pilot.py $A
 
