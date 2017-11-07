f4c3a3158d705a58e2d870beb4f69a4f2295a28a
buddhasystem <potekhin@bnl.gov>
2017-11-08 00:40:49 +0100

    Silenced one output in the script

--- htcondor/pilotclean.sh
+++ htcondor/pilotclean.sh
@@ -10,5 +10,5 @@ source /afs/cern.ch/user/m/mxp/vp3s/bin/activate
 to=`$P3S_HOME/clients/TO.py -t $P3S_PILOT_TO`
 echo timeout:$to
 
-purge=`$P3S_HOME/clients/purge.py -w pilot -s timeout`
-echo purge:$purge
+#purge=`$P3S_HOME/clients/purge.py -w pilot -s timeout`
+#echo purge:$purge
