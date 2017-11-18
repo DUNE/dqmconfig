96c7ffe85021eb93efb7ce1a80cd2c6d409c27bd
Maxim Potekhin <potekhin@bnl.gov>
2017-11-17 22:12:12 -0500

    Updated the purge shell script, and crontab

--- htcondor/pilotpurge.sh
+++ htcondor/pilotpurge.sh
@@ -8,5 +8,4 @@ source $P3S_HOME/configuration/lxvm.sh > /dev/null
 source /afs/cern.ch/user/m/mxp/vp3s/bin/activate
 
 
-purge=`$P3S_HOME/clients/purge.py -w pilot -s timeout`
-echo purge:$purge
+purge=`$P3S_HOME/clients/purge.py -w pilot -s timeout -d`
