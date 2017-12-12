3ae3565ff51047b19734437085c4a6a6a23a1dd9
Maxim Potekhin <potekhin@bnl.gov>
2017-12-12 11:13:40 -0500

    Fixes in the service script

--- services/olddata.sh
+++ services/olddata.sh
@@ -5,16 +5,27 @@ export DQM_HOME=/afs/cern.ch/user/m/mxp/projects/dqmconfig
 
 source $P3S_HOME/configuration/lxvm.sh > /dev/null
 
+
+# echo Activating venv at lxvm
+source /afs/cern.ch/user/m/mxp/vp3s/bin/activate
+export P3S_HOME=/afs/cern.ch/user/m/mxp/projects/p3s
 export outdir=$P3S_DIRPATH/output
 
-usedDisk=`du -sh $outdir`
-echo Used space before cleaning: $usedDisk
+if [ ! -d "$outdir" ]; then
+    $P3S_HOME/clients/service.py -n tscan -m "Problem with directory $outdir"
+    exit 1
+fi
+
+
+
+usedDisk1=`du -sh $outdir`
+# echo Used space before cleaning: $usedDisk1
 
 cd $outdir
 
 curdir=`pwd`
 
-echo Working: $curdir
+# echo Working: $curdir
 
 if [[ $curdir == /afs* ]]
 then
@@ -29,8 +40,8 @@ N=`find $outdir -maxdepth 1 -mindepth 1 -mmin +$DQM_DATA_LIFE -exec ls -ld  {} \
 
 find $outdir -maxdepth 1 -mindepth 1 -mmin +$DQM_DATA_LIFE -exec rm -fr  {} \;
 
-usedDisk=`du -sh $outdir`
+usedDisk2=`du -sh $outdir`
 
-message="$N directories found exceeding the data lifetime of $DQM_DATA_LIFE minutes. Used space after cleaning: $usedDisk"
+message="$N directories found exceeding the data lifetime of $DQM_DATA_LIFE minutes. Used space bedore/after cleaning: $usedDisk1/$usedDisk2"
 
 $P3S_HOME/clients/service.py -n olddata -m "$message"
