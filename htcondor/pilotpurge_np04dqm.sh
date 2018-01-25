200398f400d29c55ec41350d51e0b43d3a7b928c
Maxim Potekhin <potekhin@bnl.gov>
2018-01-25 17:05:40 -0500

    Rounded out a new cron file for the production identity

---
+++ htcondor/pilotpurge_np04dqm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export P3S_HOME=/afs/cern.ch/user/n/np04dqm/projects/p3s
+export DQM_HOME=/afs/cern.ch/user/n/np04dqm/projects/dqmconfig
+
+source $P3S_HOME/configuration/lxvm.sh > /dev/null
+
+source /afs/cern.ch/user/n/np04dqm/vp3s/bin/activate
+
+
+purge=`$P3S_HOME/clients/purge.py -w pilot -s timeout -d`
