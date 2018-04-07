ee0c9c4772f4232c504d82cc721aadf4a3ad3f15
Maxim Potekhin <potekhin@bnl.gov>
2018-04-07 18:41:02 +0200

    Adding a modified version of the scan script

---
+++ services/tscan_np04dqm.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Maxim's note - let's mute the output for now as the short
+# cron cycle leads to flooding of my mailbox
+
+export P3S_HOME=/afs/cern.ch/user/n/np04dqm/public/p3s/p3s
+export DQM_HOME=/afs/cern.ch/user/n/np04dqm/public/p3s/dqmconfig
+
+source $P3S_HOME/configuration/lxvm_np04dqm.sh > /dev/null
+
+
+Nargs=$#
+
+if [ ! $Nargs -eq 2 ]; then
+    echo Wrong number of arguments - expecting 2 - exiting...
+    echo Expecting:
+    echo \* time window \(minutes\) to trigger on a modified file, needs to be negative for "newer than" and positive for "older than"
+    echo \* wildcard or part of it e.g. Proto
+    exit
+fi
+
+# echo Activating venv at lxvm
+source $P3S_VENV/bin/activate
+
+export P3S_INPUT_DIR=$P3S_DIRPATH/input
+
+if [ ! -d "$P3S_INPUT_DIR" ]; then
+    $P3S_HOME/clients/service.py -n tscan -m "Problem with directory $P3S_INPUT_DIR"
+    exit 1
+fi
+
+cd $P3S_INPUT_DIR
+d=`pwd`
+echo Directory: $d
+files=`find . -maxdepth 1 -mindepth 1 -mmin $1 -size +1 -name "$2*" | sed 's/\.\///'`
+
+echo Files:$files
+
+$P3S_HOME/clients/service.py -n tscan -m "$files"
+
+exit
+
+# echo ${#files[@]}
+
+for f in $files
+do
+#    echo ! $f
+    $P3S_HOME/clients/dataset.py -v 0 -g -i $d -f $f -J $P3S_HOME/inputs/larsoft/lxdqm_evdisp_4.json -N
+    $P3S_HOME/clients/dataset.py -v 0 -g -i $d -f $f -J $P3S_HOME/inputs/larsoft/lxdqm_crt_tpc_3.json -N
+    $P3S_HOME/clients/dataset.py -v 0 -g -i $d -f $f -J $P3S_HOME/inputs/larsoft/lxdqm_purity_5.json
+done
+
+# env | grep P3
