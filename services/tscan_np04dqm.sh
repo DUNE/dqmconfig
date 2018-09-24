701aa750e8b371bd9dde2b6c91747f9f185a7000
Maxim Potekhin <potekhin@bnl.gov>
2018-09-24 08:38:44 -0400

    Include purity

--- services/tscan_np04dqm.sh
+++ services/tscan_np04dqm.sh
@@ -62,9 +62,10 @@ if [ ! -z "$5" ] && [ "$5" == 'T' ]; then
 fi
 
 if [ ! -z "$5" ] && [ "$5" != 'T' ]; then
-    $P3S_HOME/clients/dataset.py -v $verb -g -i $d -f $f -J $P3S_HOME/inputs/larsoft/monitor/hitmonitor_data_main.json
+    $P3S_HOME/clients/dataset.py -v $verb -g    -i $d -f $f -J $P3S_HOME/inputs/larsoft/monitor/hitmonitor_data_main.json
     $P3S_HOME/clients/dataset.py -v $verb -g -A -i $d -f $f -J $P3S_HOME/inputs/larsoft/evdisp/eventdisplay_data.json
     $P3S_HOME/clients/dataset.py -v $verb -g -A -i $d -f $f -J $P3S_HOME/inputs/larsoft/femb/fembcount_data.json
+    $P3S_HOME/clients/dataset.py -v $verb -g -A -i $d -f $f -J $P3S_HOME/inputs/larsoft/purity/purity_data.json
 fi
 let COUNTER=COUNTER+1
 if [ "$COUNTER" -eq "$4" ]; then
